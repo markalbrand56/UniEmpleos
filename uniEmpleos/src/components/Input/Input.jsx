0aadcb4d177ddd16f89b2429bda0ab34e04c01f3
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-05-02 15:57:58 -0600

    Avances en input

--- uniEmpleos/src/components/Input/Input.jsx
+++ uniEmpleos/src/components/Input/Input.jsx
@@ -1,12 +1,28 @@
 import React from "react"
-import { Input } from "./Input.module.css"
+import PropTypes from "prop-types"
+import styles from "./Input.module.css"
 
-const ComponentInput = ({ placeholder }) => {
+const ComponentInput = ({ name, value, onChange, type, placeholder }) => {
   return (
-    <div className={Input}>
-      <input type="text" placeholder={placeholder} />
+    <div className={styles.inputContainer}>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </div>
   )
+}
+
+ComponentInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
 }
 
 export default ComponentInput
--- uniEmpleos/src/pages/Login/Login.jsx
+++ uniEmpleos/src/pages/Login/Login.jsx
@@ -1,11 +1,22 @@
 import React from "react"
 import ComponentInput from "../../components/Input/Input"
+import styles from "./Login.module.css"
 
 function LogIn() {
   return (
-    <div className="App">
-      <ComponentInput placeholder="Usuario" />
-      <ComponentInput placeholder="Contraseña" />
+    <div className={styles.logInCointainer}>
+      <div>
+        <ComponentInput
+          name="usuario"
+          type="text"
+          placeholder="Ingrese su usuario"
+        />
+        <ComponentInput
+          name="password"
+          type="password"
+          placeholder="Ingrese su contraseña"
+        />
+      </div>
     </div>
   )
 }
