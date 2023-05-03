e7ac17c696b402733f046a88cf8ac06c3fa28366
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-05-03 10:06:56 -0600

    Pagina sign in

--- uniEmpleos/src/pages/Login/Login.jsx
+++ uniEmpleos/src/pages/Login/Login.jsx
@@ -21,6 +21,15 @@ function LogIn() {
         <Button label="Iniciar sesión" />
         <a href="/registro">¿Eres nuevo? Únete al equipo</a>
       </div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#B0E212"
+          fillOpacity="1"
+          d="M0,96L60,133.3C120,171,240,245,360,272C480,299,600,277,720,240C840,203,960,149,1080,
+          122.7C1200,96,1320,96,1380,96L1440,96L1440,320L1380,320C1320,320,1200,320,1080,
+          320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        />
+      </svg>
     </div>
   )
 }
