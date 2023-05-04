36765433e6f5ea11b4a72c4510136f4144341b45
Mark Albrand <mark.albrand5@gmail.com>
2023-05-04 12:45:20 -0600

    Update user.go

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,12 +1,13 @@
 package models
 
+// Usuario es la estructura de la tabla usuario en la base de datos
 type Usuario struct {
 	Usuario    string `json:"usuario"`
 	Contra     string `json:"contra"`
 	Suspendido bool   `json:"suspendido"`
 }
 
-// TableName Esta función se llama automáticamente cuando se hace un Create() en el ORM, acá va el nombre como aparece en PG
+// TableName Esta función se llama automáticamente cuando se hace un Create() en el ORM, acá va el nombre como aparece en Postgres
 func (Usuario) TableName() string {
 	return "usuario"
 }
