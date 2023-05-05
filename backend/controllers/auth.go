d6617b51481d73ae30e4ef2bd00544c18d70d092
Mark Albrand <mark.albrand5@gmail.com>
2023-05-05 00:26:44 -0600

    Crear tokens

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"backend/configs"
 	"backend/models"
 	"backend/responses"
+	"backend/utils"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterInput struct {
@@ -34,4 +37,65 @@ func Register(c *gin.Context) {
 	}
 
 	c.JSON(200, responses.StandardResponse{Status: 200, Message: "Usuario created successfully", Data: nil})
+}
+
+type LoginInput struct {
+	Usuario string `json:"usuario"`
+	Contra  string `json:"contra"`
+}
+
+func Login(c *gin.Context) {
+	var input LoginInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(400, responses.StandardResponse{Status: 400, Message: "Invalid input", Data: nil})
+		return
+	}
+
+	u := models.Usuario{
+		Usuario: input.Usuario,
+		Contra:  input.Contra,
+	}
+
+	token, err := login(u)
+
+	if err != nil {
+		c.JSON(400, responses.StandardResponse{
+			Status:  400,
+			Message: "Invalid credentials: " + err.Error(),
+			Data:    nil,
+		})
+		return
+	}
+
+	c.JSON(200, responses.StandardResponse{
+		Status:  200,
+		Message: "Login successful",
+		Data:    map[string]interface{}{"token": token},
+	})
+}
+
+func login(usuario models.Usuario) (string, error) {
+	var err error
+
+	found := models.Usuario{}
+	err = configs.DB.Where("usuario = ?", usuario.Usuario).First(&found).Error
+
+	if err != nil {
+		return "", err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(found.Contra), []byte(usuario.Contra))
+
+	if err != nil {
+		return "", err
+	}
+
+	token, err := utils.GenerateToken(found.Usuario)
+
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
--- backend/routes/routes_handler.go
+++ backend/routes/routes_handler.go
@@ -8,5 +8,6 @@ import (
 func Routes(router *gin.Engine) {
 	router.POST("/upload", controllers.UploadFile())
 	router.POST("/register", controllers.Register)
+	router.POST("/login", controllers.Login)
 	router.POST("/chat", controllers.SendMessage)
 }
