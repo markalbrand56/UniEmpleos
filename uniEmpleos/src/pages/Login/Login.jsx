8672975b732afd906c1a593f9d7254f407c4c9ef
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-05-12 16:51:05 -0600

    connection

--- uniEmpleos/src/pages/Login/Login.jsx
+++ uniEmpleos/src/pages/Login/Login.jsx
@@ -1,30 +1,87 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { useStoreon } from "storeon/react"
 import ComponentInput from "../../components/Input/Input"
 import Button from "../../components/Button/Button"
 import styles from "./Login.module.css"
+import Popup from "../../components/Popup/Popup"
+import API_URL from "../../api"
 
 const LogIn = () => {
+  const [emailInput, setEmailInput] = useState("")
+  const [passInput, setPassInput] = useState("")
+  const [warning, setWarning] = useState(false)
+
+  const { dispatch } = useStoreon("user")
+
+  // Teniendo el DPI y la contraseña,necesitamos que nos devuelva un objeto usuario
+  const logIn = async () => {
+    console.log("dpi: ", emailInput)
+    console.log("pass: ", passInput)
+    console.log(`API URL: ${API_URL}`)
+    const body = {
+      usuario: emailInput,
+      contra: passInput,
+    }
+    const response = await fetch(`${API_URL}login/`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    const datos = await response.json() // Recibidos
+
+    if (datos.status === 200) {
+      // Estado global
+      dispatch("user/login", { token: datos.data.token })
+      history.push("/")
+    } else {
+      console.log("Credenciales incorrectas")
+      setWarning(() => true)
+    }
+  }
+
+  useEffect(() => {
+    dispatch("user/login", { token: null })
+  }, [])
+
   return (
     <div className={styles.logInCointainer}>
+      {warning && (
+        <Popup
+          message="Credenciales incorrectas. Inténtelo de nuevo"
+          setWarning={setWarning}
+          closable
+        />
+      )}
       <h1>UniEmpleos</h1>
       <div className={styles.inputsContainer}>
         <div className={styles.usuarioContainer}>
           <span>Ingrese su correo</span>
-          <ComponentInput
+          <input
             name="correo"
             type="text"
             placeholder="jim@gmail.com"
+            onChange={(e) => setEmailInput(e.target.value)}
           />
         </div>
-        <div className={styles.contrasenaContainer}>
+        <div className={styles.usuarioContainer}>
           <span>Ingrese su contraseña</span>
-          <ComponentInput
+          <input
             name="password"
             type="password"
             placeholder="micontraseña123"
+            onChange={(e) => setPassInput(e.target.value)}
           />
         </div>
-        <Button label="Iniciar sesión" />
+        <Button
+          label="Iniciar sesión"
+          onClick={(event) => {
+            event.preventDefault()
+            logIn()
+          }}
+        />
         <a href="/signup">
           ¿Eres nuevo? <span> Únete al equipo </span>
         </a>
