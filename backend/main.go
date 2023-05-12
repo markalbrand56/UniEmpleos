bde0e3423754aa7f46ab0f98fbb83aec978e0df0
Mark Albrand <mark.albrand5@gmail.com>
2023-05-12 17:29:45 -0600

    CORS

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"backend/configs"
 	"backend/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.Default())
 
 	routes.Routes(router)
 	configs.SetupDB()
