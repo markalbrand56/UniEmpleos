aab88ba279efbdf83e3a7d2f00dd91ffc656086f
Mark Albrand <mark.albrand5@gmail.com>
2023-05-12 16:48:50 -0600

    CORS

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"backend/configs"
 	"backend/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "http://localhost:3000" || origin == "http://localhost:8080"
+		},
+	}))
 
 	routes.Routes(router)
 	configs.SetupDB()
