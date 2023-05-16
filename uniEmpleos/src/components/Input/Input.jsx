c9551efc8aba558d01545eea4da18c0f684d01ea
angelcast2002 <castellanosa80@gmail.com>
2023-05-16 14:39:49 -0600

    add: arreglo de componentes sin funciones para interactuar con ellos

--- uniEmpleos/src/components/Input/Input.jsx
+++ uniEmpleos/src/components/Input/Input.jsx
@@ -2,10 +2,16 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./Input.module.css"
 
-const ComponentInput = ({ name, type, placeholder }) => {
+const ComponentInput = ({ name, type, placeholder, onChange }) => {
   return (
     <div className={styles.inputContainer}>
-      <input id={name} name={name} type={type} placeholder={placeholder} />
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
     </div>
   )
 }
@@ -14,6 +20,7 @@ ComponentInput.propTypes = {
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
 
 export default ComponentInput
