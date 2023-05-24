a4fc3c2d2390474d2ffe631114eaf682fb15700f
Mark Albrand <mark.albrand5@gmail.com>
2023-05-24 01:25:37 -0600

    Update api.js

--- uniEmpleos/src/api.js
+++ uniEmpleos/src/api.js
@@ -1,2 +1,4 @@
-const API_URL = "http://ec2-3-101-148-58.us-west-1.compute.amazonaws.com"
+const API_URL = "http://ec2-13-57-35-73.us-west-1.compute.amazonaws.com"
+// const API_URL = "http://13.57.35.73"
+
 export default API_URL
