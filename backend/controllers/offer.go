1b55ab7cfb2e4b78bd1aac086cbb144519bc6ab6
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-05-24 21:11:56 -0600

    Oferta y Oferta_Carrera
    Ya funciona jajajajaja SUIIIIIIIIIIIIIIIII

--- backend/controllers/offer.go
+++ backend/controllers/offer.go
@@ -9,6 +9,16 @@ import (
 )
 
 type OfferInput struct {
+	IDEmpresa   string   `json:"id_empresa"`
+	Puesto      string   `json:"puesto"`
+	Descripcion string   `json:"descripcion"`
+	Requisitos  string   `json:"requisitos"`
+	Salario     float64  `json:"salario"`
+	IdCarreras  []string `json:"id_carreras"`
+}
+
+type AfterInsert struct {
+	IdOferta    int     `json:"id_oferta"`
 	IDEmpresa   string  `json:"id_empresa"`
 	Puesto      string  `json:"puesto"`
 	Descripcion string  `json:"descripcion"`
@@ -16,10 +26,15 @@ type OfferInput struct {
 	Salario     float64 `json:"salario"`
 }
 
+type AfterInsert2 struct {
+	IdOferta  int `json:"id_oferta"`
+	IdCarrera int `json:"id_carrera"`
+}
+
 func NewOffer(c *gin.Context) {
 	var input OfferInput
 
-	if err := c.ShouldBindJSON(&input); err != nil {
+	if err := c.BindJSON(&input); err != nil {
 		c.JSON(400, responses.StandardResponse{
 			Status:  400,
 			Message: "Invalid input: " + err.Error(),
@@ -38,7 +53,8 @@ func NewOffer(c *gin.Context) {
 
 	fmt.Println(offer)
 
-	err := configs.DB.Create(&offer).Error
+	var inserted AfterInsert
+	err := configs.DB.Raw("INSERT INTO oferta (id_empresa, puesto, descripcion, requisitos, salario) VALUES (?, ?, ?, ?, ?) RETURNING id_oferta, id_empresa, puesto, descripcion, requisitos, salario", offer.IDEmpresa, offer.Puesto, offer.Descripcion, offer.Requisitos, offer.Salario).Scan(&inserted).Error
 
 	if err != nil {
 		c.JSON(400, responses.StandardResponse{
@@ -49,103 +65,26 @@ func NewOffer(c *gin.Context) {
 		return
 	}
 
+	fmt.Println("\ncarreras: ", input.IdCarreras)
+
+	// Insert into oferta_carrera table
+	for _, idCarrera := range input.IdCarreras {
+		var inserted2 AfterInsert2
+		err = configs.DB.Raw("INSERT INTO oferta_carrera (id_oferta, id_carrera) VALUES (?, ?) RETURNING id_oferta, id_carrera", inserted.IdOferta, idCarrera).Scan(&inserted2).Error
+		if err != nil {
+			c.JSON(400, responses.StandardResponse{
+				Status:  400,
+				Message: "Error creating oferta_carrera: " + err.Error(),
+				Data:    nil,
+			})
+			return
+		}
+	}
+
 	c.JSON(200, responses.StandardResponse{
 		Status:  200,
-		Message: "Offer created successfully",
+		Message: "Offer and oferta_carrera created successfully",
 		Data:    nil,
 	})
 
-}
-
-type OfferGet struct {
-	Id_Oferta string `json:"id_oferta"`
-}
-
-func GetOffer(c *gin.Context) {
-	var offer models.OfertaGet
-	var Company models.Empresa
-	var data map[string]interface{}
-	var input OfferGet
-
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(400, responses.StandardResponse{
-			Status:  400,
-			Message: "Invalid input: " + err.Error(),
-			Data:    nil,
-		})
-		return
-	}
-
-	err := configs.DB.Where("id_oferta = ?", input.Id_Oferta).First(&offer).Error
-
-	if err != nil {
-		c.JSON(400, responses.StandardResponse{
-			Status:  400,
-			Message: "Error getting offer: " + err.Error(),
-			Data:    nil,
-		})
-		return
-	}
-
-	err = configs.DB.Where("id_empresa = ?", offer.IDEmpresa).First(&Company).Error
-
-	if err != nil {
-		c.JSON(400, responses.StandardResponse{
-			Status:  400,
-			Message: "Error getting company: " + err.Error(),
-			Data:    nil,
-		})
-		return
-	}
-
-	data = map[string]interface{}{
-		"offer":   offer,
-		"company": Company,
-	}
-
-	c.JSON(200, responses.StandardResponse{
-		Status:  200,
-		Message: "Offer retrieved successfully",
-		Data:    data,
-	})
-}
-
-type GetOfferByCompanyInput struct {
-	Id_Empresa string `json:"id_empresa"`
-}
-
-func GetOfferByCompany(c *gin.Context) {
-	var offers []models.OfertaGet
-	var data map[string]interface{}
-	var input GetOfferByCompanyInput
-
-	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(400, responses.StandardResponse{
-			Status:  400,
-			Message: "Invalid input: " + err.Error(),
-			Data:    nil,
-		})
-		return
-	}
-
-	err := configs.DB.Where("id_empresa = ?", input.Id_Empresa).Find(&offers).Error
-
-	if err != nil {
-		c.JSON(400, responses.StandardResponse{
-			Status:  400,
-			Message: "Error getting offers: " + err.Error(),
-			Data:    nil,
-		})
-		return
-	}
-
-	data = map[string]interface{}{
-		"offers": offers,
-	}
-
-	c.JSON(200, responses.StandardResponse{
-		Status:  200,
-		Message: "Offers retrieved successfully",
-		Data:    data,
-	})
 }
