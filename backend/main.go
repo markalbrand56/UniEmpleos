5f14f0228aa27ac4d2c65d69d31388bb5883996b
Mark Albrand <mark.albrand5@gmail.com>
2023-05-25 15:59:07 -0600

    Rollback

--- backend/main.go
+++ backend/main.go
@@ -10,17 +10,10 @@ import (
 func main() {
 	router := gin.Default()
 
-	// Configurar el middleware CORS
-	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
-	config.AddAllowHeaders("Authorization")
-	config.AddAllowMethods("OPTIONS") // Agregar OPTIONS como método permitido
-	router.Use(cors.New(config))
+	router.Use(cors.Default())
 
 	routes.Routes(router)
 	configs.SetupDB()
-
-	router.OPTIONS("/*any", func(c *gin.Context) {}) // Agregar OPTIONS como método permitido
 
 	router.Run(":8080")
 }
