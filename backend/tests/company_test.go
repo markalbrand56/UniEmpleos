3828e70686dd792b931431b1a52ba116a2f5a414
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-07-22 09:48:05 -0600

    update: test still not working

--- backend/tests/company_test.go
+++ backend/tests/company_test.go
@@ -12,12 +12,15 @@ import (
 func TestUpdateCompany(t *testing.T) {
 	router := setupRouter()
 	w := httptest.NewRecorder()
+	r := httptest.NewRecorder()
 
 	// Login needed to get token
 	jsonDataPrev := `{"usuario": "reclutamiento@sarita.com", "contra": "sarita"}`
 	bodyPrev := bytes.NewBufferString(jsonDataPrev)
 	reqPrev := httptest.NewRequest("POST", "/api/login", bodyPrev)
-	router.ServeHTTP(w, reqPrev)
+	router.ServeHTTP(r, reqPrev)
+
+	// var token = r.Body.buf
 
 	jsonData := `{"nombre": "Sarita SA", "detalles": "Dummy 2", "correo": "reclutamiento@sarita.com", "telefono": "22227314", "contra": "sarita"}`
 	body := bytes.NewBufferString(jsonData)
@@ -25,6 +28,7 @@ func TestUpdateCompany(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	fmt.Println(w.Body.String())
+	fmt.Println(r.Body.String())
 	// Comprueba la respuesta HTTP y el cuerpo de la respuesta
 	assert.Equal(t, http.StatusOK, w.Code, "Status code is not 200")
 }
