5c424f3f17f5eb9f2731facac100f86a1b1cb577
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-07-22 09:38:58 -0600

    update test working, but not on the file is supposed to.

--- backend/tests/controller_test.go
+++ backend/tests/controller_test.go
@@ -56,23 +56,3 @@ func TestLogin(t *testing.T) {
 	// Comprueba la respuesta HTTP y el cuerpo de la respuesta
 	assert.Equal(t, http.StatusOK, w.Code, "Status code is not 200")
 }
-
-func TestUpdateCompany(t *testing.T) {
-	router := setupRouter()
-	w := httptest.NewRecorder()
-
-	// Login needed to get token
-	jsonDataPrev := `{"usuario": "reclutamiento@sarita.com", "contra": "sarita"}`
-	bodyPrev := bytes.NewBufferString(jsonDataPrev)
-	reqPrev := httptest.NewRequest("POST", "/api/login", bodyPrev)
-	router.ServeHTTP(w, reqPrev)
-
-	jsonData := `{"nombre": "Sarita SA", "detalles": "Dummy 2", "correo": "reclutamiento@sarita.com", "telefono": "22227314", "contra": "sarita"}`
-	body := bytes.NewBufferString(jsonData)
-	req := httptest.NewRequest("PUT", "/api/companies/update", body)
-	router.ServeHTTP(w, req)
-
-	fmt.Println(w.Body.String())
-	// Comprueba la respuesta HTTP y el cuerpo de la respuesta
-	assert.Equal(t, http.StatusOK, w.Code, "Status code is not 200")
-}
