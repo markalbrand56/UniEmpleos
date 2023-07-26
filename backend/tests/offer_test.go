3127d673fb64253eaaab1b84d5abf8f12691adfd
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-07-25 19:48:56 -0600

    Test 3 (Create new offer) done and passed

--- backend/tests/offer_test.go
+++ backend/tests/offer_test.go
@@ -85,40 +85,40 @@ func TestCompanyOffers(t *testing.T) {
 	fmt.Println("************* Testing Results END *************")
 }
 
-//func TestNewOffer(t *testing.T) {
-//	// api/offers/
-//	// Create a new offer.
-//
-//	router := setupRouter()
-//	w := httptest.NewRecorder()
-//
-//	jsonData := `{"usuario": "reclutamiento@sarita.com", "contra": "sarita"}`
-//	body := bytes.NewBufferString(jsonData)
-//	req := httptest.NewRequest("POST", "/api/login", body)
-//	router.ServeHTTP(w, req)
-//
-//	var loginResponse struct {
-//		Status  int    `json:"status"`
-//		Message string `json:"message"`
-//		Data    struct {
-//			Role  string `json:"role"`
-//			Token string `json:"token"`
-//		}
-//	}
-//
-//	err := json.Unmarshal(w.Body.Bytes(), &loginResponse)
-//	assert.NoError(t, err, "Error unmarshalling login response")
-//
-//	jsonData2 := `{"id_empresa":"reclutamiento@sarita.com", "puesto":"puesto dummy", "descripcion":"descripcion dummy", "requisitos":"requisitos dummy", "salario":1000.00, "id_carreras":[1, 2, 3]}`
-//	body2 := bytes.NewBufferString(jsonData2)
-//	req2 := httptest.NewRequest("POST", "/api/offers/", body2)
-//	req2.Header.Set("Authorization", "Bearer "+loginResponse.Data.Token)
-//
-//	router.ServeHTTP(w, req2)
-//
-//	fmt.Println("************* Creating a new Offer *************")
-//	fmt.Println(w.Body.String())
-//	assert.Equal(t, http.StatusOK, w.Code, "Status code is not 200 on new offer")
-//	fmt.Println("************* Testing Results END *************")
-//
-//}
+func TestNewOffer(t *testing.T) {
+	// api/offers/
+	// Create a new offer.
+
+	router := setupRouter()
+	w := httptest.NewRecorder()
+
+	jsonData := `{"usuario": "reclutamiento@sarita.com", "contra": "sarita"}`
+	body := bytes.NewBufferString(jsonData)
+	req := httptest.NewRequest("POST", "/api/login", body)
+	router.ServeHTTP(w, req)
+
+	var loginResponse struct {
+		Status  int    `json:"status"`
+		Message string `json:"message"`
+		Data    struct {
+			Role  string `json:"role"`
+			Token string `json:"token"`
+		}
+	}
+
+	err := json.Unmarshal(w.Body.Bytes(), &loginResponse)
+	assert.NoError(t, err, "Error unmarshalling login response")
+
+	jsonData2 := `{"id_empresa":"reclutamiento@sarita.com", "puesto":"puesto dummy", "descripcion":"descripcion dummy", "requisitos":"requisitos dummy", "salario":1000.00, "id_carreras":["1", "2", "3"]}`
+	body2 := bytes.NewBufferString(jsonData2)
+	req2 := httptest.NewRequest("POST", "/api/offers/", body2)
+	req2.Header.Set("Authorization", "Bearer "+loginResponse.Data.Token)
+
+	router.ServeHTTP(w, req2)
+
+	fmt.Println("************* Creating a new Offer *************")
+	fmt.Println(w.Body.String())
+	assert.Equal(t, http.StatusOK, w.Code, "Status code is not 200 on new offer")
+	fmt.Println("************* Testing Results END *************")
+
+}
