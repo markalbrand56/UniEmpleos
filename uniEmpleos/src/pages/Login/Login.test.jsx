fc9c9f1207941151a817823b239eac231e8cfd6a
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-11-14 18:35:11 -0600

    fixed

---
+++ uniEmpleos/src/pages/Login/Login.test.jsx
@@ -0,0 +1,53 @@
+import { mount, test, describe } from "vitest"
+import { StoreContext } from "storeon/react"
+import { store } from "../../store" // Asegúrate de importar tu store
+import LogIn from "./Login"
+import { getByTestId } from "@testing-library/dom"
+
+test("LogIn component", () => {
+  test("should render without crashing", () => {
+    const { getByText } = mount(
+      <StoreContext.Provider value={store}>
+        <LogIn />
+      </StoreContext.Provider>
+    )
+
+    test.expect(getByText("UniEmpleos")).toBeInTheDocument()
+  })
+
+  test("should update state when inputs change", () => {
+    const { getByPlaceholderText } = mount(
+      <StoreContext.Provider value={store}>
+        <LogIn />
+      </StoreContext.Provider>
+    )
+
+    getByPlaceholderText("uni@gmail.com").value = "test@gmail.com"
+    getByPlaceholderText("micontraseña123").value = "password123"
+
+    test
+      .expect(getByPlaceholderText("uni@gmail.com").value)
+      .toBe("test@gmail.com")
+    test
+      .expect(getByPlaceholderText("micontraseña123").value)
+      .toBe("password123")
+  })
+
+  test("should display warning when login fails", async () => {
+    const { getByText, getByPlaceholderText } = mount(
+      <StoreContext.Provider value={store}>
+        <LogIn />
+      </StoreContext.Provider>
+    )
+
+    getByPlaceholderText("uni@gmail.com").value = "test@gmail.com"
+    getByPlaceholderText("micontraseña123").value = "wrongpassword"
+    getByTestId("Iniciar sesión").click()
+
+    await test.waitFor(() => {
+      test
+        .expect(getByText("Credenciales incorrectas. Inténtelo de nuevo"))
+        .toBeInTheDocument()
+    })
+  })
+})
