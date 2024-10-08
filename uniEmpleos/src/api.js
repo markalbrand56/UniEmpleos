54ca17f4cee275d1c2acbe89331a0eb8fff167dc
Mark Albrand <mark.albrand5@gmail.com>
2024-10-08 10:46:28 -0600

    Update

--- uniEmpleos/src/api.js
+++ uniEmpleos/src/api.js
@@ -1,5 +1,5 @@
 // const API_URL = "http://ec2-13-57-35-73.us-west-1.compute.amazonaws.com"
 // const API_URL = "http://13.57.35.73"
-const API_URL = "https://whole-letisha-markalbrand56.koyeb.app"
+const API_URL = import.meta.env.VITE_BACKEND_URL
 
 export default API_URL
